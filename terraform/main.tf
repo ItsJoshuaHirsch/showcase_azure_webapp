64a03a7979c7c0d308b7ce560266898a65308372
Joshua Hirsch <40173207+ItsJoshuaHirsch@users.noreply.github.com>
2024-12-17 08:53:24 +0100

    cleanup, create simple inf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,57 +14,32 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "demo_rg" {
-  name     = "demo-resource-group"
-  location = "West Europe"
+  name     = "razor-pages-us-rg"
+  location = "East US"
 }
 
 # App Service
-resource "azurerm_app_service_plan" "demo_plan" {
-  name                = "demo-app-service-plan"
+resource "azurerm_app_service_plan" "demo_app_plan" {
+  name                = "razor-pages-app-service-plan"
   location            = azurerm_resource_group.demo_rg.location
   resource_group_name = azurerm_resource_group.demo_rg.name
   sku {
-    tier = "Free" # "Standard" or higher for scaling...
+    tier = "Free"
     size = "F1"
   }
 }
 
 resource "azurerm_app_service" "demo_app" {
-  name                = format("demo-web-app-%s", "test-hirsch-demo")
+  name                = format("demo-web-app-%s", "test-hirsch-demo-newtf")
   location            = azurerm_resource_group.demo_rg.location
   resource_group_name = azurerm_resource_group.demo_rg.name
-  app_service_plan_id = azurerm_app_service_plan.demo_plan.id
+  app_service_plan_id = azurerm_app_service_plan.demo_app_plan.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
 }
 
-# Create a Storage Account
-resource "azurerm_storage_account" "frontend_storage" {
-  name                     = "demowebfrontendstorage"
-  resource_group_name      = azurerm_resource_group.demo_rg.name
-  location                 = azurerm_resource_group.demo_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  https_traffic_only_enabled = true
-}
-
-resource "azurerm_storage_account_static_website" "frontend_static" {
-  storage_account_id = azurerm_storage_account.frontend_storage.id
-  error_404_document = "index.html"
-  index_document     = "index.html"
-}
-
-# Out
 output "app_service_url" {
   value = azurerm_app_service.demo_app.default_site_hostname
-}
-
-output "app_service_name" {
-  value = azurerm_app_service.demo_app.name
-}
-
-output "frontend_static_site_url" {
-  value = azurerm_storage_account.frontend_storage.primary_web_endpoint
 }
