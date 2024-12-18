b861b789f0654ffb6829c0b33a6a05d5679c2b0b
Joshua Hirsch <40173207+ItsJoshuaHirsch@users.noreply.github.com>
2024-12-18 16:48:19 +0100

    upgrade from app service plan to service plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,21 +19,24 @@ resource "azurerm_resource_group" "demo_rg" {
 }
 
 # App Service
-resource "azurerm_app_service_plan" "demo_app_plan" {
+resource "azurerm_service_plan" "demo_app_plan" {
+  os_type  = "Windows"
+  sku_name = "F1"     # Free = F1, Scalable = S1
   name                = "showcase-web-app-service-plan"
   location            = azurerm_resource_group.demo_rg.location
   resource_group_name = azurerm_resource_group.demo_rg.name
-  sku {
-    tier = "Free"     # Standard, Free
-    size = "F1"       # S1, F1
-  }
+  #sku {
+  #  tier = "Free"     # Standard, Free
+  #  size = "F1"       # S1, F1
+  #}
+
 }
 
 resource "azurerm_app_service" "demo_app" {
   name                = format("showcase-web-app-%s", "hirsch")
   location            = azurerm_resource_group.demo_rg.location
   resource_group_name = azurerm_resource_group.demo_rg.name
-  app_service_plan_id = azurerm_app_service_plan.demo_app_plan.id
+  app_service_plan_id = azurerm_service_plan.demo_app_plan.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
